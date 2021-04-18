8b79c9d590553c9f84d5c403c45f4a47e5d5dd43
Popostatopulos <egorpostovalov@gmail.com>
2021-04-18 21:13:05 +0500

    Осталось отформатировать таблицу и запустить на linux

--- task1.py
+++ task1.py
@@ -1,13 +1,15 @@
 import re
-import sys
 import socket
 
-from sys import platform
-from typing import List
+from dataclasses import dataclass
 from subprocess import Popen, PIPE
+from sys import platform
+from typing import List, Tuple
+from multiprocessing.pool import ThreadPool
+from itertools import zip_longest
 
 
-class Parser:
+class UtilityParser:
     @classmethod
     def parse_output(cls, target: str):
         if platform == 'linux' or platform == 'linux2' or platform == 'darwin':
@@ -16,11 +18,11 @@ class Parser:
             return cls._parse_tracert_output(target)
 
     @staticmethod
-    def _parse_traceroute_output(target: str) -> List[str]:
+    def _parse_traceroute_output(target: str) -> List[bytes]:
         pass
 
     @staticmethod
-    def _parse_tracert_output(target: str) -> List[str]:
+    def _parse_tracert_output(target: str) -> List[bytes]:
         split_regex = re.compile(b'\s+')
         ip_regex = re.compile(b'^\d{1,3}\\.\d{1,3}\\.\d{1,3}\\.\d{1,3}$')
         with Popen(f'tracert -d {target}', shell=True, stdout=PIPE) as proc:
@@ -29,41 +31,119 @@ class Parser:
                 for res in re.split(split_regex, line):
                     ip = re.search(ip_regex, res)
                     if ip:
-                        result.append(ip.string.decode())
+                        result.append(ip.string)
 
         return result
 
 
 class WhoisParser:
-    NREFER = 'nrefer:'
-
-    def __init__(self):
-        pass
+    _REFER = 'refer:'
+    _COUNTRY = b'country:'
+    _ORIGIN = b'origin:'
+    _ROLE = b'role:'
 
     @classmethod
     def parse_IANA_answer(cls, answer: str) -> str:
         for line in answer.split('\n'):
-            if line.startswith(cls.NREFER):
-                return line.replace(cls.NREFER, '').replace(' ', '').strip()
+            if line.startswith(cls._REFER):
+                return line.replace(cls._REFER, '').strip()
+
+    @classmethod
+    def parse_regional_registrar_answer(
+            cls, answer: bytes) -> Tuple[str, str, str]:
+        country, origin, role = '', '', ''
+        for line in answer.split(b'\n'):
+            if line.startswith(cls._COUNTRY):
+                country = line.replace(cls._COUNTRY, b'').strip()
+            elif line.startswith(cls._ORIGIN):
+                origin = line.replace(cls._ORIGIN, b'').strip()
+            elif line.startswith(cls._ROLE):
+                role = line.replace(cls._ROLE, b'').strip()
+
+        encoding = 'utf-8'
+        return (country.decode(encoding),
+                origin.decode(encoding),
+                role.decode(encoding))
 
 
 class WhoisQuestioner:
-    def __init__(self, addresses):
-        self.addresses = addresses
-        self.port = 43
-        self.host_IANA = socket.gethostbyname('whois.iana.org.')
+    GLOBAL_REGISTRAR = 'whois.iana.org.'
 
-    def ask(self):
-        ip_with_registrator = {}
+    @dataclass
+    class _RequiredInfo:
+        def __init__(self, index: int,
+                     number_of_AS: str, country: str, provider: str):
+            self.index: int = index
+            self.AS_number: str = number_of_AS
+            self.country: str = country
+            self.provider: str = provider
+
+        def __str__(self):
+            return f'{self.index}, {self.AS_number}, {self.country}, {self.provider}'
+
+    def __init__(self, addresses):
+        self.addresses: List[bytes] = addresses
+        self.port: int = 43
+        self.host_IANA: str = socket.gethostbyname(self.GLOBAL_REGISTRAR)
+        self.ip_with_info = {}
+
+    def build_table(self):
+        with ThreadPool() as pool:
+            results = [
+                pool.apply_async(
+                    self._ask_regional_registrar, args=(self._ask_IANA(ip), ip)
+                ) for ip in self.addresses
+            ]
+
+            for i in range(len(results)):
+                ip = self.addresses[i]
+                self.ip_with_info[ip] = self._RequiredInfo(
+                    i + 1, *results[i].get()
+                )
+
+        self._print_table()
+
+    def _print_table(self):
+        lines = []
+        columns_len = {
+            "Порядковый номер": 0, "IP-адрес": 0, "AS": 0,
+            "Страна": 0, "Провайдер": 0
+        }
+
+        for ip in self.ip_with_info:
+            info = self.ip_with_info[ip]
+            lines.append(
+                (info.index, ip, info.AS_number, info.country, info.provider)
+            )
+
+        # Что-то сложно...
+
+
+    def _ask_IANA(self, ip) -> str:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as m_socket:
             m_socket.connect((self.host_IANA, self.port))
-            for ip in self.addresses:
-                m_socket.sendall(b'\n\r')
-            data = m_socket.recv(1024)
-            print(repr(data))
+            m_socket.sendall(b'%b\r\n' % ip)
+            data = m_socket.recv(65536)
+
+            return WhoisParser.parse_IANA_answer(data.decode(encoding='utf-8'))
+
+    def _ask_regional_registrar(self, registrar, ip) -> Tuple[str, str, str]:
+        if not registrar:
+            return '', '', ''
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((socket.gethostbyname(registrar), self.port))
+            sock.sendall(b'%b\r\n' % ip)
+            full_answer = b''
+            while True:
+                data = sock.recv(65536)
+                if not data:
+                    break
+                full_answer += data
+
+        return WhoisParser.parse_regional_registrar_answer(full_answer)
 
 
 if __name__ == '__main__':
-    WhoisQuestioner(['as']).ask()
-    # questioner = WhoisQuestioner(Parser.parse_output('ya.ru'))
-    # questioner.ask()
+    # WhoisQuestioner(['as']).ask()
+    questioner = WhoisQuestioner(UtilityParser.parse_output('ya.ru'))
+    questioner.build_table()
